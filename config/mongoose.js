2eaad7a524401102ac5aedf87a291a5d0747bcb2
Aman garg <aman0944.cse19@chitkara.edu.in>
2022-06-18 15:48:57 +0530

    hacu

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose=require('mongoose');
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.DATABASE}`);
+mongoose.connect(process.env.MONGODB_URL || `mongodb://localhost:27017/${process.env.DATABASE}`);
 
 const db=mongoose.connection
 
